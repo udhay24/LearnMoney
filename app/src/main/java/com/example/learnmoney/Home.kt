a7620b3d44e9af5e8c6a7a048076eaffe66532f7
udhay24 <udhayakumar241998@gmail.com>
2019-06-23 10:05:22 +0530

    added profile picture

--- app/src/main/java/com/example/learnmoney/Home.kt
+++ app/src/main/java/com/example/learnmoney/Home.kt
@@ -3,7 +3,11 @@ package com.example.learnmoney
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.drawerlayout.widget.DrawerLayout
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.nav_header_main.*
 
 class Home : AppCompatActivity() {
 
