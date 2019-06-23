b30d791c419a677b9d328f04236d9fdd4a254155
udhay24 <udhayakumar241998@gmail.com>
2019-06-23 08:38:03 +0530

    added intent

--- app/src/main/java/com/example/learnmoney/Home.kt
+++ app/src/main/java/com/example/learnmoney/Home.kt
@@ -1,12 +1,18 @@
 package com.example.learnmoney
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home.*
 
 class Home : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        mutual_funds_root.setOnClickListener {
+            startActivity(Intent(this@Home, MFMune::class.java))
+        }
     }
 }
